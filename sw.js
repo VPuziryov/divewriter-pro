a14ed15c21331972ba58db89bb0a07ff7a4e73b6
Vitaly Puziryov <v.puziryov@gmail.com>
2026-05-29 03:55:28 +0300

    sw

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installed');
+self.addEventListener('install', event => {
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activated');
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', () => {
+  // no cache yet
 });
