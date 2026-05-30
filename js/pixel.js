47a24f1593a17e9e0e3f97bc4495f2081aa739da
Vitaly Puziryov <v.puziryov@gmail.com>
2026-05-30 05:15:41 +0300

    30/05-005

--- js/pixel.js
+++ js/pixel.js
@@ -1,7 +1,80 @@
-function trackEvent(eventName) {
+(function () {
 
-  console.log('Pixel event:', eventName);
+  if (!PRODUCT.metaPixel) {
+    return;
+  }
+
+  !(function(f,b,e,v,n,t,s)
+  {
+    if(f.fbq)return;
+
+    n=f.fbq=function(){
+
+      n.callMethod
+        ? n.callMethod.apply(n,arguments)
+        : n.queue.push(arguments);
+
+    };
+
+    if(!f._fbq)f._fbq=n;
+
+    n.push=n;
+    n.loaded=true;
+    n.version='2.0';
+    n.queue=[];
+
+    t=b.createElement(e);
+    t.async=true;
+    t.src=v;
+
+    s=b.getElementsByTagName(e)[0];
+
+    s.parentNode.insertBefore(t,s);
+
+  })(window,
+     document,
+     'script',
+     'https://connect.facebook.net/en_US/fbevents.js');
+
+  fbq('init', PRODUCT.metaPixel);
+
+  fbq('track', 'PageView');
+
+})();
+
+function trackEvent(eventName, params = {}) {
+
+  if (typeof fbq === 'function') {
+
+    fbq('track', eventName, params);
+
+  }
 
 }
 
-trackEvent('PageView');
+const currentPath = window.location.pathname;
+
+if (
+  currentPath === "/" ||
+  currentPath.endsWith("/index.html")
+) {
+
+  trackEvent("ViewContent");
+
+}
+
+if (
+  currentPath.includes("/go")
+) {
+
+  trackEvent("InitiateCheckout");
+
+}
+
+if (
+  currentPath.includes("/app")
+) {
+
+  trackEvent("Purchase");
+
+}
